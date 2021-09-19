5868f891f6551c3a6195a7c43a7ce7b40f7c90af
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-09-19 13:42:00 +0200

    improve GraalVM reflection;Fix log

--- src/main/java/io/qiot/manufacturing/factory/facilitymanager/service/core/CoreServiceImpl.java
+++ src/main/java/io/qiot/manufacturing/factory/facilitymanager/service/core/CoreServiceImpl.java
@@ -8,11 +8,18 @@ import javax.enterprise.event.Event;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
+import org.jboss.resteasy.plugins.providers.DocumentProvider;
+import org.jboss.resteasy.plugins.providers.FormUrlEncodedProvider;
+import org.jboss.resteasy.plugins.providers.IIOImageProvider;
+import org.jboss.resteasy.plugins.providers.JaxrsFormProvider;
+import org.jboss.resteasy.plugins.providers.SourceProvider;
+import org.jboss.resteasy.plugins.providers.sse.SseEventProvider;
 import org.slf4j.Logger;
 
 import io.qiot.manufacturing.all.commons.domain.cdi.BootstrapCompletedEventDTO;
 import io.qiot.manufacturing.factory.facilitymanager.service.factory.FactoryService;
 import io.quarkus.runtime.StartupEvent;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 
 /**
  * @author andreabattaglia
@@ -20,6 +27,9 @@ import io.quarkus.runtime.StartupEvent;
  */
 // @Startup(1)
 @ApplicationScoped
+@RegisterForReflection(targets = { IIOImageProvider.class,
+        SseEventProvider.class, FormUrlEncodedProvider.class,
+        SourceProvider.class, DocumentProvider.class, JaxrsFormProvider.class })
 public class CoreServiceImpl implements CoreService {
 
     @Inject
